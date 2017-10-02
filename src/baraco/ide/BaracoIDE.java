af7745aa7da49c73f1a2bbe4b4a3320f2ba8aef3
Kevin <kevin98gray@gmail.com>
2017-10-02 23:35:01 +0800

    Add line numbers

--- src/baraco/ide/BaracoIDE.java
+++ src/baraco/ide/BaracoIDE.java
@@ -1,6 +1,9 @@
 package baraco.ide;
 
 import javax.swing.*;
+import javax.swing.event.DocumentEvent;
+import javax.swing.event.DocumentListener;
+import javax.swing.text.Element;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -14,6 +17,7 @@ public class BaracoIDE {
     private JScrollPane scrollPaneConsole;
     private JButton buttonFile;
     private JButton buttonEdit;
+    private JTextArea lineNumbers;
 
 
     public BaracoIDE() {
@@ -22,6 +26,33 @@ public class BaracoIDE {
             public void actionPerformed(ActionEvent e) {
                 System.out.println("Run clicked");
             }
+        });
+
+        editor.getDocument().addDocumentListener(new DocumentListener(){
+            public String getText(){
+                int caretPosition = editor.getDocument().getLength();
+                Element root = editor.getDocument().getDefaultRootElement();
+                String text = "1" + System.getProperty("line.separator");
+                for(int i = 2; i < root.getElementIndex( caretPosition ) + 2; i++){
+                    text += i + System.getProperty("line.separator");
+                }
+                return text;
+            }
+            @Override
+            public void changedUpdate(DocumentEvent de) {
+                lineNumbers.setText(getText());
+            }
+
+            @Override
+            public void insertUpdate(DocumentEvent de) {
+                lineNumbers.setText(getText());
+            }
+
+            @Override
+            public void removeUpdate(DocumentEvent de) {
+                lineNumbers.setText(getText());
+            }
+
         });
     }
 
