28506bba83497684a70d3049c4847140b0769336
Kevin <kevin98gray@gmail.com>
2017-10-03 17:19:16 +0800

    Add antlr-package

---
+++ src/baraco/controller/Controller.java
@@ -0,0 +1,14 @@
+package baraco.controller;
+
+import javax.swing.*;
+
+public class Controller {
+
+    public void run(String sourceCode, JTextArea console) {
+        // Perform interpretation
+
+        String output = "Test output";
+
+        console.append(output);
+    }
+}
