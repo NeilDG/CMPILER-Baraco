0d94d0f49c6afab3724eefe0fc093de1dd95c606
Kevin <kevin98gray@gmail.com>
2017-10-03 18:17:23 +0800

    Connect IDE to scanner

--- src/baraco/controller/Controller.java
+++ src/baraco/controller/Controller.java
@@ -1,14 +1,33 @@
 package baraco.controller;
 
+import moka.lexer.MokaLexer;
+import org.antlr.v4.runtime.ANTLRInputStream;
+import org.antlr.v4.runtime.CharStream;
+import org.antlr.v4.runtime.CommonTokenStream;
+import org.antlr.v4.runtime.Token;
+
 import javax.swing.*;
+import java.util.List;
 
 public class Controller {
 
-    public void run(String sourceCode, JTextArea console) {
+    public void run(String input, JTextArea console) {
         // Perform interpretation
 
-        String output = "Test output";
+        CharStream cs = new ANTLRInputStream(input);
 
-        console.append(output);
+        MokaLexer mokaLexer = new MokaLexer(cs);
+
+        CommonTokenStream tokenStream = new CommonTokenStream(mokaLexer);
+
+        tokenStream.fill();
+
+        List<Token> tokens = tokenStream.getTokens();
+
+        String output = "";
+        for(int i = 0; i < tokens.size(); i++)
+            output += tokens.get(i).getText() + "\n";
+
+        console.append("\n" + output);
     }
 }
