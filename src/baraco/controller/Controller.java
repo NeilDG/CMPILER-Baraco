b3a3c63cf19460df56be7b78299f4dc7b3a20cfe
Kevin <kevin98gray@gmail.com>
2017-10-17 21:36:39 +0800

    Fix errors, parse tree works

--- src/baraco/controller/Controller.java
+++ src/baraco/controller/Controller.java
@@ -1,14 +1,12 @@
 package baraco.controller;
 
-<<<<<<< HEAD
 import moka.lexer.BaracoLexer;
+import moka.parser.BaracoBaseListener;
+import moka.parser.BaracoListener;
 import moka.parser.BaracoParser;
-=======
-import moka.lexer.MokaLexer;
-import moka.parser.MokaBaseListener;
-import moka.parser.MokaListener;
-import moka.parser.MokaParser;
->>>>>>> master
+import org.antlr.v4.runtime.CharStream;
+
+
 import org.antlr.v4.runtime.ANTLRInputStream;
 import org.antlr.v4.runtime.CharStream;
 import org.antlr.v4.runtime.CommonTokenStream;
@@ -40,12 +38,12 @@ public class Controller {
 
 
 
-        MokaParser parser = new MokaParser(tokenStream);
+        BaracoParser parser = new BaracoParser(tokenStream);
 
         ParseTree tree = parser.compilationUnit();
 
         ParseTreeWalker walker = new ParseTreeWalker();
-        MokaListener listener = new MokaBaseListener();
+        BaracoListener listener = new BaracoBaseListener();
         walker.walk(listener, tree);
 
         /*String output = "";
@@ -63,14 +61,6 @@ public class Controller {
 
         console.append("\n" + output);*/
 
-<<<<<<< HEAD
-        BaracoLexer parser = new BaracoLexer(tokenStream);
-
-        ParseTree tree = parser.compilationUnit();
-
-
-=======
->>>>>>> master
         //TESTED PRINT FUNCTION
 
         /*console.setText("");
