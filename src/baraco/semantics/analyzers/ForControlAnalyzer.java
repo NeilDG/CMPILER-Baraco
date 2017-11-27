bbe0beff3db9601c37e24f8c4298bf48906738a6
Kevin <kevin98gray@gmail.com>
2017-11-27 19:32:00 +0800

    Allow shorthand commands in for loop update command

--- src/baraco/semantics/analyzers/ForControlAnalyzer.java
+++ src/baraco/semantics/analyzers/ForControlAnalyzer.java
@@ -2,14 +2,17 @@ package baraco.semantics.analyzers;
 
 import baraco.antlr.lexer.BaracoLexer;
 import baraco.antlr.parser.BaracoParser;
+import baraco.antlr.parser.BaracoParser.ExpressionContext;
+import baraco.antlr.parser.BaracoParser.ForUpdateContext;
+import baraco.antlr.parser.BaracoParser.LocalVariableDeclarationContext;
 import baraco.execution.commands.ICommand;
 import baraco.execution.commands.evaluation.AssignmentCommand;
+import baraco.execution.commands.evaluation.ShorthandCommand;
 import baraco.execution.commands.simple.IncDecCommand;
 import org.antlr.v4.runtime.ParserRuleContext;
 import org.antlr.v4.runtime.tree.ErrorNode;
 import org.antlr.v4.runtime.tree.ParseTreeListener;
 import org.antlr.v4.runtime.tree.ParseTreeWalker;
-import baraco.antlr.parser.BaracoParser.*;
 import org.antlr.v4.runtime.tree.TerminalNode;
 
 public class ForControlAnalyzer implements ParseTreeListener {
@@ -51,6 +54,21 @@ public class ForControlAnalyzer implements ParseTreeListener {
             if(StatementExpressionAnalyzer.isAssignmentExpression(exprCtx)) {
                 //this.updateCommand = new AssignmentCommand(exprCtx.expression(0), exprCtx.expression(1));
                 this.updateCommand = new AssignmentCommand(exprCtx.expression(0), exprCtx.expression(1));
+            }
+            else if(StatementExpressionAnalyzer.isAddAssignExpression(exprCtx)) {
+                this.updateCommand = new ShorthandCommand(exprCtx.expression(0), exprCtx.expression(1), BaracoLexer.ADD_ASSIGN);
+            }
+            else if(StatementExpressionAnalyzer.isSubAssignExpression(exprCtx)) {
+                this.updateCommand = new ShorthandCommand(exprCtx.expression(0), exprCtx.expression(1), BaracoLexer.SUB_ASSIGN);
+            }
+            else if(StatementExpressionAnalyzer.isMulAssignExpression(exprCtx)) {
+                this.updateCommand = new ShorthandCommand(exprCtx.expression(0), exprCtx.expression(1), BaracoLexer.MUL_ASSIGN);
+            }
+            else if(StatementExpressionAnalyzer.isDivAssignExpression(exprCtx)) {
+                this.updateCommand = new ShorthandCommand(exprCtx.expression(0), exprCtx.expression(1), BaracoLexer.DIV_ASSIGN);
+            }
+            else if(StatementExpressionAnalyzer.isModAssignExpression(exprCtx)) {
+                this.updateCommand = new ShorthandCommand(exprCtx.expression(0), exprCtx.expression(1), BaracoLexer.MOD_ASSIGN);
             }
             else if(StatementExpressionAnalyzer.isIncrementExpression(exprCtx)) {
                 this.updateCommand = new IncDecCommand(exprCtx.expression(0), BaracoLexer.INC);
